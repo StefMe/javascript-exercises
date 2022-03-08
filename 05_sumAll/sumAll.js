017f43e50a6ce12ea2d655d8f671c68b6a8f6cab
Steffen Menk <steffenmenk@gmail.com>
2022-03-08 17:18:52 +0100

    Task 5 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (startingNumber, endingNumber) {
+  if (
+    typeof startingNumber != "number" ||
+    typeof endingNumber != "number" ||
+    startingNumber < 0 ||
+    endingNumber < 0
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
+  let cache = 0;
+  if (endingNumber < startingNumber) {
+    cache = startingNumber;
+    startingNumber = endingNumber;
+    endingNumber = cache;
+  }
+  for (let index = startingNumber; index <= endingNumber; index++) {
+    sum = sum + index;
+  }
+  return sum;
 };
 
 // Do not edit below this line
