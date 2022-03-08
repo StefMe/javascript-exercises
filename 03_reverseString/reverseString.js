8f6071b8c7accf7bd235fde6bf1fabda480c39a0
Steffen Menk <steffenmenk@gmail.com>
2022-03-08 09:57:24 +0100

    Task 3 Done - reverseString.js modified to fulfil requirements - reverseString.spec.js modified to not skip tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+  let cacheString = stringToReverse.split("");
+  cacheString = cacheString.reverse();
+  return cacheString.join("");
 };
 
 // Do not edit below this line
