b05ecff2b262fdc407dd730f5d4a4ca24bfef22a
Steffen Menk <steffenmenk@gmail.com>
2022-03-09 14:00:24 +0100

    Task 9 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (wordToCheck) {
+  wordToCheck = wordToCheck.toLowerCase();
+  wordToCheck = wordToCheck.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "");
+  let reverseWordArray = wordToCheck.split("");
+  reverseWordArray = reverseWordArray.reverse();
+  let reverseWord = reverseWordArray.join("");
+  return reverseWord === wordToCheck ? true : false;
 };
 
 // Do not edit below this line
