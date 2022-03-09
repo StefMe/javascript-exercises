7ef0935084c0a475f7a529f2212a55673932e9f5
Steffen Menk <steffenmenk@gmail.com>
2022-03-09 05:59:45 +0100

    Task 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (tempInC) {
+  return Math.round((((tempInC - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempInF) {
+  return Math.round(((tempInF * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
