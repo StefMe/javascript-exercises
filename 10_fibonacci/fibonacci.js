1e967166130a95493baa14edc5b0aab643c35873
Steffen Menk <steffenmenk@gmail.com>
2022-03-09 14:14:45 +0100

    Task 10 Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibonacciNumber) {
+  if (fibonacciNumber < 0) {
+    return "OOPS";
+  }
+  let fibonacciCache = [];
+  for (let index = 0; index < fibonacciNumber; index++) {
+    if (index < 2) {
+      fibonacciCache.push(1);
+    } else {
+      fibonacciCache.push(
+        fibonacciCache[index - 1] + fibonacciCache[index - 2]
+      );
+    }
+  }
+  return fibonacciCache[fibonacciNumber - 1];
 };
 
 // Do not edit below this line
