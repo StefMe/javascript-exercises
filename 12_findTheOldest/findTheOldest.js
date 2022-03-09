a0c41056e8435fc94e4398c399b86194014a41a6
Steffen Menk <steffenmenk@gmail.com>
2022-03-09 16:52:30 +0100

    Task 12 Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (objectToSearch) {
+  let maxYearLived;
+  let oldestPerson;
 
+  let ageArray = objectToSearch.map((el) => {
+    maxYearLived = el.hasOwnProperty("yearOfDeath")
+      ? el.yearOfDeath
+      : new Date().getFullYear();
+    return maxYearLived - el.yearOfBirth;
+  });
+  let arrayPosition = ageArray.indexOf(Math.max(...ageArray));
+  let objectName = objectToSearch[arrayPosition];
+  return objectName;
 };
 
 // Do not edit below this line
